b4ebca48fcedfcdc98cddd0a667c52a8c01a7acd
dnj-isme <85777096+dnj-isme@users.noreply.github.com>
2021-11-04 20:40:54 +0700

    smoothing everything

--- AMDP 1/js/validate.js
+++ AMDP 1/js/validate.js
@@ -27,3 +27,53 @@ function validateLogin()
         return true;
     }
 }
+
+function validateRegister() {
+    let errorDiv = document.getElementById("error");
+    let name = document.forms["register"]["name"].value;
+    let email = document.forms["register"]["email"].value;
+    let gender = document.forms["register"]["gender"].value;
+    let city = document.forms["register"]["city"].value;
+    let password = document.forms["register"]["password"].value;
+    let conf = document.forms["register"]["conf"].value;
+
+    errorDiv.innerHTML = "";
+
+    if(name == "") {
+        errorDiv.innerHTML = "Fill your name";
+        return false;
+    }
+    else if(email == "") {
+        errorDiv.innerHTML = "Fill your email";
+        return false;
+    }
+    else if(gender == "") {
+        errorDiv.innerHTML = "Fill your gender";
+        return false;
+    }
+    else if(city == "") {
+        errorDiv.innerHTML = "Fill your city";
+        return false;
+    }
+    else if(password == "") {
+        errorDiv.innerHTML = "Fill your password";
+        return false;
+    }
+    else {
+        var check = false;
+        check = password.length >= 8 && /[A-Z]/.test(password) && /[0-9]/.test(password);
+        
+        if(!check) {
+            errorDiv.innerHTML = "Your password need at least 8 characters with a number and a capital letter";
+            return false;
+        }
+
+        if(password != conf) {
+            errorDiv.innerHTML = "Those passwords didn’t match. Try again.";
+            return false;
+        } else {
+            alert("Validation successful");
+            return true;
+        }
+    }
+}
